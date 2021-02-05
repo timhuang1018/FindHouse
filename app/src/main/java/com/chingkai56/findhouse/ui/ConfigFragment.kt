27d43c44364955b7f015e50372ee9416f8bd6089
TimHuang <t8522192@gmail.com>
2021-02-05 18:16:09 +0800

    import androidx preference library

---
+++ app/src/main/java/com/chingkai56/findhouse/ui/ConfigFragment.kt
@@ -0,0 +1,15 @@
+package com.chingkai56.findhouse.ui
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+/**
+ * Created by timhuang on 2021/2/5.
+ **/
+
+class ConfigFragment():PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+    }
+}
